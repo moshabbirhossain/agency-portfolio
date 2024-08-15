0463e4b95836c88d7b5290869adee52d8258e6ce
Walton <hossainmoshabbir337@gmail.com>
2024-08-15 13:07:15 +0600

    images not showing problem finally solved

--- src/components/Portfolios/Portfolios.jsx
+++ src/components/Portfolios/Portfolios.jsx
@@ -6,7 +6,7 @@ const Portfolios = () => {
             </h1>
             <div className="grid grid-cols-3 gap-10 grid-flow-row-dense">
                 <div className="p-6 border border-gray-200 rounded-lg ">
-                <img src="/public/assets/github.svg" alt="" className="w-7 h-7 text-gray-50 dark:text-gray-400 mb-3"/>
+                <img src="/assets/github.svg" alt="" className="w-7 h-7 text-gray-50 dark:text-gray-400 mb-3"/>
                     <a href="#">
                         <h5 className="mb-2 text-2xl font-semibold tracking-tight text-white">
                         Deploy from GitHub
@@ -15,10 +15,10 @@ const Portfolios = () => {
                     <p className="mb-3 font-normal text-white">
                     Simply git push, we build and deploy your app with blazing fast built-in continuous deployment. Develop fearlessly with native versioning of all deployments.
                     </p>
-                    <img src="/public/assets/g2.webp" alt=""/>
+                    <img src="/assets/g2.webp" alt=""/>
                 </div>
                 <div className="p-6 border border-gray-200 rounded-lg ">
-                    <img src="/public/assets/docker.svg" alt="" className="w-7 h-7 text-gray-50 dark:text-gray-400 mb-3"/>
+                    <img src="/assets/docker.svg" alt="" className="w-7 h-7 text-gray-50 dark:text-gray-400 mb-3"/>
                     <a href="#">
                         <h5 className="mb-2 text-2xl font-semibold tracking-tight text-white">
                         Deploy Docker containers
@@ -27,10 +27,10 @@ const Portfolios = () => {
                     <p className="mb-3 font-normal text-white">
                     Build Docker containers, host them on any registry, and atomically deploy your new version worldwide in a single API call.
                     </p>
-                    <img src="/public/assets/d2.webp" alt="" />
+                    <img src="/assets/d2.webp" alt="" />
                 </div>
                 <div className="p-6 border border-gray-200 rounded-lg ">
-                <img src="/public/assets/team.svg" alt="" className="w-7 h-7 text-gray-50 dark:text-gray-400 mb-3"/>
+                <img src="/assets/team.svg" alt="" className="w-7 h-7 text-gray-50 dark:text-gray-400 mb-3"/>
                     <a href="#">
                         <h5 className="mb-2 text-2xl font-semibold tracking-tight text-white">
                         Develop with your team
@@ -39,7 +39,7 @@ const Portfolios = () => {
                     <p className="mb-3 font-normal text-white">
                     Invite your team to build together and enjoy live preview after each push with built-in CI/CD.
                     </p>
-                    <img src="/public/assets/t2.webp" alt="" />
+                    <img src="/assets/t2.webp" alt="" />
                 </div>
             </div>
         </div>
